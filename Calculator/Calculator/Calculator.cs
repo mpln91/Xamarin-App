ce899ec49ab167aeec3f6eb8464ef897dd5d7639
Lakshmi Narayanan Manikkam Prabhakar <mpln007@gmail.com>
2018-01-27 20:41:01 -0500

    Add function for add button.

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Calculator
+{
+    public class Calculator : ICalculator
+    {
+        public Calculator()
+        {
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
--- Calculator/Calculator/ViewController.cs
+++ Calculator/Calculator/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Foundation;
 using UIKit;
 
 namespace Calculator
@@ -8,13 +8,29 @@ namespace Calculator
     {
         protected ViewController(IntPtr handle) : base(handle)
         {
-            // Note: this .ctor should not contain any initialization logic.
         }
 
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+            AddButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                // Convert the phone number with text to a number
+                // using PhoneTranslator.cs
+                var a = Int32.Parse(InputFieldA.Text);
+                var b = Int32.Parse(InputFieldB.Text);
+
+                InputFieldA.ResignFirstResponder();
+                InputFieldB.ResignFirstResponder();
+                var result = "";
+                var m_calculator = new Calculator();
+                if(string.IsNullOrWhiteSpace(a.ToString()) && string.IsNullOrWhiteSpace(b.ToString()))
+                    result = m_calculator.Add(a, b).ToString();
+
+                OutputField.Text = result;
+                    
+            };
         }
 
         public override void DidReceiveMemoryWarning()
